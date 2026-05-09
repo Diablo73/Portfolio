db41b149a7329c2ae72829ed8c3f3937eaafc410
Diablo73 <ashul2173@gmail.com>
2026-05-09 15:39:39 +0530

    particles js

--- portfolio-app/src/pages/home/Home.js
+++ portfolio-app/src/pages/home/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initParticles } from '../../js/particles.js';
 
 function Home() {
+	useEffect(() => {
+		initParticles();
+	}, []);
+
 	return (
-		<div className="splash-container" style={{ animation: 'fadeIn 2s ease-in-out' }}>
-			<a href="https://www.example.com">Visit Example</a>
-			<h1>Hello World</h1>
+		<div id="home" style={{ animation: 'fadeIn 2s ease-in-out' }}>
+			<section className="center">
+				<div id="particles-js"></div>
+			</section>
+			<div className="splash-container">
+				<a href="https://www.example.com">Visit Example</a>
+				<h1>Hello World</h1>
+			</div>
 		</div>
+
 	);
 }
 
